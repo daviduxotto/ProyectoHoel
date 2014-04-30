2261b8cf37dfed44e61374015c688d0ad85c2aca
Daviduxotto <daviduxotto@gmail.com>
2014-04-30 16:41:38 -0600

    Modulo de administracion de habitaciones
    completado unn 40%

---
+++ ProyectoHotel/habitaciones.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace ProyectoHotel
+{
+    partial class habitaciones
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle31 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle32 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle33 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.Codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tipo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NumeroCamas = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TipoCama = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Capacidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tarifa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BotonEliminar = new System.Windows.Forms.Button();
+            this.BotonAñadir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.BackgroundColor = System.Drawing.Color.Teal;
+            this.dataGrid.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dataGridViewCellStyle31.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle31.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            dataGridViewCellStyle31.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle31.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle31.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle31.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle31.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle31;
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Codigo,
+            this.Tipo,
+            this.NumeroCamas,
+            this.TipoCama,
+            this.Capacidad,
+            this.Tarifa});
+            dataGridViewCellStyle32.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle32.BackColor = System.Drawing.Color.MintCream;
+            dataGridViewCellStyle32.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle32.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle32.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            dataGridViewCellStyle32.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle32.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGrid.DefaultCellStyle = dataGridViewCellStyle32;
+            this.dataGrid.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGrid.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.dataGrid.Location = new System.Drawing.Point(134, 1);
+            this.dataGrid.Name = "dataGrid";
+            dataGridViewCellStyle33.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle33.BackColor = System.Drawing.Color.Teal;
+            dataGridViewCellStyle33.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle33.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle33.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle33.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle33.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGrid.RowHeadersDefaultCellStyle = dataGridViewCellStyle33;
+            this.dataGrid.Size = new System.Drawing.Size(645, 415);
+            this.dataGrid.TabIndex = 0;
+            // 
+            // Codigo
+            // 
+            this.Codigo.HeaderText = "Codigo";
+            this.Codigo.Name = "Codigo";
+            // 
+            // Tipo
+            // 
+            this.Tipo.HeaderText = "Tipo";
+            this.Tipo.Name = "Tipo";
+            // 
+            // NumeroCamas
+            // 
+            this.NumeroCamas.HeaderText = "Numero de Camas";
+            this.NumeroCamas.Name = "NumeroCamas";
+            // 
+            // TipoCama
+            // 
+            this.TipoCama.HeaderText = "Tipo de Cama";
+            this.TipoCama.Name = "TipoCama";
+            // 
+            // Capacidad
+            // 
+            this.Capacidad.HeaderText = "Capacidad";
+            this.Capacidad.Name = "Capacidad";
+            // 
+            // Tarifa
+            // 
+            this.Tarifa.HeaderText = "Tarifa";
+            this.Tarifa.Name = "Tarifa";
+            // 
+            // BotonEliminar
+            // 
+            this.BotonEliminar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BotonEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BotonEliminar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.BotonEliminar.Image = global::ProyectoHotel.Properties.Resources.delete;
+            this.BotonEliminar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.BotonEliminar.Location = new System.Drawing.Point(3, 81);
+            this.BotonEliminar.Name = "BotonEliminar";
+            this.BotonEliminar.Size = new System.Drawing.Size(125, 63);
+            this.BotonEliminar.TabIndex = 2;
+            this.BotonEliminar.Text = "Eliminar Habitacion";
+            this.BotonEliminar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.BotonEliminar.UseVisualStyleBackColor = false;
+            // 
+            // BotonAñadir
+            // 
+            this.BotonAñadir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BotonAñadir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BotonAñadir.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.BotonAñadir.Image = global::ProyectoHotel.Properties.Resources.add;
+            this.BotonAñadir.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.BotonAñadir.Location = new System.Drawing.Point(3, 12);
+            this.BotonAñadir.Name = "BotonAñadir";
+            this.BotonAñadir.Size = new System.Drawing.Size(125, 63);
+            this.BotonAñadir.TabIndex = 1;
+            this.BotonAñadir.Text = "Añadir Habitacion";
+            this.BotonAñadir.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.BotonAñadir.UseVisualStyleBackColor = false;
+            // 
+            // habitaciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(776, 406);
+            this.Controls.Add(this.BotonEliminar);
+            this.Controls.Add(this.BotonAñadir);
+            this.Controls.Add(this.dataGrid);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "habitaciones";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hotel Santa Barbara - Habitaciones";
+            this.Load += new System.EventHandler(this.habitaciones_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Codigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tipo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NumeroCamas;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TipoCama;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Capacidad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tarifa;
+        private System.Windows.Forms.Button BotonAñadir;
+        private System.Windows.Forms.Button BotonEliminar;
+    }
+}
